6cbeb8c9d78216437e7fdc50c069c53e7adb8775
miles-tails-prower05 <kunwoopark05@gmail.com>
2025-05-10 21:49:29 +0900

    타이틀, 아이콘 변경

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
-from tkinter import *
+﻿from tkinter import *
 
 root = Tk()
 root.geometry("600x600")
+root.title("이미지 출력 테스트")
+root.configure(bg="white")
+
+icon = PhotoImage(file='image/sumung.png')
+root.iconphoto(True, icon)
 
 canvas = Canvas(root, width=540, height=540)
 canvas.pack()
